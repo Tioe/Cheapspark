2979d8ae11c4dff881b2add003748d49a368ae03
Tioe <koen.degreef@gmail.com>
2015-12-04 04:53:28 +0100

    Modified relay code and cleaned up code
    Relay code: Added check for topic, Sends 50msec pulse instead of switching
    Clean up code: Bundled code by type/sensor, renamed and removed variables

--- Cheapspark.ino
+++ Cheapspark.ino
@@ -1,30 +1,148 @@
+// WIFI (ESSENTIAL)
 #include <espduino.h>
-#include <mqtt.h>
-#include <dht.h>
-
 #define MYSSID "tim"
-#define MYPASS "PASSWORD"
-#define BROKERIP "192.168.1.121"
-#define MQTTCLIENT "cheapspark1"
-#define MQTTSTOPIC0 "commands"
+#define MYPASS "password"
+ESP esp(&Serial, 4);                          //Uses digital Pin 4 for sleep mode
+boolean wifiConnected = false;
+
+
+// MQTT (ESSENTIAL)
+#include <mqtt.h>
+#define BROKERIP "192.168.0.213"
+#define MQTTCLIENT "cheapspark0"
+#define MQTT_TOPIC_DEBUG "/cheapspark0/debug"        //Debug info
+MQTT mqtt(&esp);
+
+
+// DHT
+#include <dht.h>
 #define DHT_PIN 5
+#define DHT_SEND_INTERVAL 15000      // Send data each 15 seconds
+#define MQTT_TOPIC_HUMI "/cheapspark0/dht22/humi"   //output
+#define MQTT_TOPIC_TEMP "/cheapspark0/dht22/temp"   //output
+unsigned long nextDHTPub = DHT_SEND_INTERVAL;
+dht DHT;
+
+
+// Relay
 #define REL1_PIN 9
 #define REL2_PIN 10
 #define REL3_PIN 11
 #define REL4_PIN 12
+#define MQTT_TOPIC_RELAYS "/cheapspark0/relays"       //Input send r1, r2, r3 or r4
 
 
-ESP esp(&Serial, 4);
-MQTT mqtt(&esp);
-boolean wifiConnected = false;
-int reportInterval =  15000;
-unsigned long now = 0;
-unsigned long nextPub = reportInterval;
-int ledpin = 13;
-boolean ledstate = false;
+// Switch
+#define SWITCH_PIN 0              //Uses analog pin 0
+#define SWITCH_PIN_D A0           //Needed because pin 0 is also accessed digital
+#define MQTT_TOPIC_SWITCH "/cheapspark0/switch"       //Input send r1, r2, r3 or r4
 boolean switchstate = false;
-dht DHT;
 
+
+// Nano Led for debugging
+#define LED_PIN 13
+boolean ledstate = false;
+
+
+// Common Loop Timer
+unsigned long now = 0;
+
+
+///////////////////////////////////
+///////////////////////////////////
+// Main Setup                    //
+///////////////////////////////////
+///////////////////////////////////
+void setup() 
+{
+  // Switch Setup
+  switchstate = false;
+  pinMode(SWITCH_PIN_D,INPUT);
+  digitalWrite(SWITCH_PIN_D,HIGH);
+  
+  // Relay Setup
+  pinMode(REL1_PIN,OUTPUT);
+  pinMode(REL2_PIN,OUTPUT);
+  pinMode(REL3_PIN,OUTPUT);
+  pinMode(REL4_PIN,OUTPUT);
+  digitalWrite(REL1_PIN,HIGH);
+  digitalWrite(REL2_PIN,HIGH);
+  digitalWrite(REL3_PIN,HIGH);
+  digitalWrite(REL4_PIN,HIGH);
+
+  // Wifi Setup
+  delay(5000);      //LAGER PROBEREN
+  Serial.begin(19200);
+  esp.enable();
+  delay(500);
+  esp.reset();
+  delay(500);
+  while(!esp.ready());
+
+  //MQTT Setup
+  if(!mqtt.begin(MQTTCLIENT, "", "", 30, 1)) while(1);      //Wait till MQTT begins
+  mqtt.lwt(MQTT_TOPIC_DEBUG, MQTTCLIENT " offline (lwt)", 0, 0);  //setup mqtt lwt
+  mqtt.connectedCb.attach(&mqttConnected);                  //setup mqtt events
+  mqtt.disconnectedCb.attach(&mqttDisconnected);
+  mqtt.publishedCb.attach(&mqttPublished);
+  mqtt.dataCb.attach(&mqttData);
+
+  // Wifi Finish Setup
+  esp.wifiCb.attach(&wifiCb);
+  esp.wifiConnect(MYSSID,MYPASS);
+}
+
+
+///////////////////////////////////
+///////////////////////////////////
+// Main Loop                     //
+///////////////////////////////////
+///////////////////////////////////
+void loop()
+{
+  esp.process();  
+  if(wifiConnected)
+  {
+    now = millis();
+    
+    // Switch
+    int switchval = analogRead(SWITCH_PIN);
+    if ((switchval>500) && (switchstate == false))
+    {
+      mqtt.publish(MQTT_TOPIC_SWITCH,"Switch1On");
+      switchstate = !switchstate;
+    }
+    if ((switchval<500) && (switchstate == true))
+    {
+      mqtt.publish(MQTT_TOPIC_SWITCH,"Switch1Off");
+      switchstate = !switchstate;
+    }
+    
+    // DHT publishing loop
+    if (now >= nextDHTPub)                  //COULD GO WRONG AFTER 49 DAYS
+    { 
+      nextDHTPub = now + DHT_SEND_INTERVAL;
+      
+      int chk = DHT.read22(DHT_PIN);
+      float humid = DHT.humidity;
+      float tempe = DHT.temperature;
+      
+      char chHumid[10];
+      char chTempe[10];
+      
+      dtostrf(humid,1,2,chHumid);
+      dtostrf(tempe,1,2,chTempe);
+      
+      mqtt.publish(MQTT_TOPIC_HUMI,chHumid);
+      mqtt.publish(MQTT_TOPIC_TEMP,chTempe);
+    }
+  }
+}
+
+
+///////////////////////////////////
+// WIFI FUNCTIONS                //
+///////////////////////////////////
 void wifiCb(void* response)
 {
   uint32_t status;
@@ -39,23 +157,24 @@ void wifiCb(void* response)
       wifiConnected = false;
       mqtt.disconnect();
     }
-    
   }
 }
 
+
+///////////////////////////////////
+// MQTT FUNCTIONS                //
+///////////////////////////////////
 void mqttConnected(void* response)
 {
-  delay(500);
-  mqtt.subscribe(""/" MQTTCLIENT "/" MQTTSTOPIC0");
-  mqtt.publish("/fb", MQTTCLIENT " online");
+  delay(500);  // Weg doen?
+  mqtt.publish(MQTT_TOPIC_DEBUG, MQTTCLIENT " online");
+  mqtt.subscribe(MQTT_TOPIC_RELAYS);
 }
-
 
 void mqttDisconnected(void* response)
 {
 
 }
-
 
 void mqttData(void* response)
 {
@@ -63,97 +182,39 @@ void mqttData(void* response)
   char buffer[4];
   String topic = res.popString();
   String data = res.popString();
-//  ledstate = !ledstate;
-//  digitalWrite(ledpin, ledstate);
-//  if (topic = "/Cheapspark1/commands") {
-    data.toCharArray(buffer,4);
-    if (strcmp(buffer,"r11") == 0) digitalWrite(REL1_PIN,HIGH);
-    if (strcmp(buffer,"r10") == 0) digitalWrite(REL1_PIN,LOW);
-
-    if (strcmp(buffer,"r21") == 0) digitalWrite(REL2_PIN,HIGH);
-    if (strcmp(buffer,"r20") == 0) digitalWrite(REL2_PIN,LOW);
-
-    if (strcmp(buffer,"r31") == 0) digitalWrite(REL3_PIN,HIGH);
-    if (strcmp(buffer,"r30") == 0) digitalWrite(REL3_PIN,LOW);
-
-    if (strcmp(buffer,"r41") == 0) digitalWrite(REL4_PIN,HIGH);
-    if (strcmp(buffer,"r40") == 0) digitalWrite(REL4_PIN,LOW);
-
+  data.toCharArray(buffer,4);
+  
+  // Nano Led for debugging
+  ledstate = !ledstate;
+  digitalWrite(LED_PIN, ledstate);
+  
+  // Debug info
+  //mqtt.publish(MQTT_TOPIC_DEBUG, MQTTCLIENT " recieved a msg:" );
+  
+  // Relay
+  if (topic.compareTo(MQTT_TOPIC_RELAYS) == 0)        //Relay Command received?
+  {    
+    if (strcmp(buffer,"r1") == 0) {
+      digitalWrite(REL1_PIN,LOW);
+      delay(50);
+      digitalWrite(REL1_PIN,HIGH);
+    } else if (strcmp(buffer,"r2") == 0) {
+      digitalWrite(REL2_PIN,LOW);
+      delay(50);
+      digitalWrite(REL2_PIN,HIGH);
+    } else if (strcmp(buffer,"r3") == 0) {
+      digitalWrite(REL3_PIN,LOW);
+      delay(50);
+      digitalWrite(REL3_PIN,HIGH);
+    } else if (strcmp(buffer,"r4") == 0) {
+      digitalWrite(REL4_PIN,LOW);
+      delay(50);
+      digitalWrite(REL4_PIN,HIGH);
+    }
+  }
 }
-void mqttPublished(void* response)
+
+void mqttPublished(void* response)        //runs when publish is a success
 {
-//runs when publish is a success
-}
-void setup() {
-  switchstate = false;
-  pinMode(A0,INPUT);
-  digitalWrite(A0,HIGH);
-  pinMode(REL1_PIN,OUTPUT);
-  pinMode(REL2_PIN,OUTPUT);
-  pinMode(REL3_PIN,OUTPUT);
-  pinMode(REL4_PIN,OUTPUT);
-  
-  
-  delay(5000);
-  Serial.begin(19200);
-  esp.enable();
-  delay(500);
-  esp.reset();
-  delay(500);
-  while(!esp.ready());
 
-//setup mqtt client");
-  if(!mqtt.begin(MQTTCLIENT, "", "", 30, 1)) {
-    while(1);
-  }
-
-//setup mqtt lwt
-  mqtt.lwt("/lwt", MQTTCLIENT " offline", 0, 0);
-  
-/*setup mqtt events */
-  mqtt.connectedCb.attach(&mqttConnected);
-  mqtt.disconnectedCb.attach(&mqttDisconnected);
-  mqtt.publishedCb.attach(&mqttPublished);
-  mqtt.dataCb.attach(&mqttData);  
-  
-  
-  /*setup wifi*/
-  esp.wifiCb.attach(&wifiCb);
-  esp.wifiConnect(MYSSID,MYPASS);
-
-}
-
-void loop() {
-  esp.process();
-
-  if(wifiConnected) {
-    now = millis();
-    int switchval = analogRead(0);
-
-    if (now >= nextPub) {
-      
-      nextPub = reportInterval + now;
-      
-      int chk = DHT.read22(DHT_PIN);
-      float humid = DHT.humidity;
-      float tempe = DHT.temperature;
-      
-      char chHumid[10];
-      char chTempe[10];
-
-      dtostrf(humid,1,2,chHumid);
-      dtostrf(tempe,1,2,chTempe);
-
-      mqtt.publish(("/" MQTTCLIENT "/humi"),chHumid);
-      mqtt.publish(("/" MQTTCLIENT "/temp"),chTempe);
-    }
-    if ((switchval>500) && (switchstate == false)) {
-      mqtt.publish(("/" MQTTCLIENT "/" MQTTSTOPIC0),"s11");
-      switchstate = !switchstate;
-    }
-    if ((switchval<500) && (switchstate == true)) {
-      mqtt.publish(("/" MQTTCLIENT "/" MQTTSTOPIC0),"s10");
-      switchstate = !switchstate;
-    }
-  }
 }
